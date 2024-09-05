ddd9512d8a026cf032718c93c1144cd302c945f0
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2024-09-05 23:36:16 +0500

    3nd commit

--- Lecture 1/hello.cpp
+++ Lecture 1/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main() {
-    int a = 10;
-    cout << a << endl;
+    int a = 10,b=100;
+    cout << a/b << endl;
     cout << "Hello, World!" << endl;
     return 0;
 }
